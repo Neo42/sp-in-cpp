ef951907ee2f749da0bac2cf381e3b86111d43e6
Johnny Jiang <22409868+Neo42@users.noreply.github.com>
2024-02-15 00:02:05 +1030

    feat: rewrite pop order

--- stacks/pop_order.cpp
+++ stacks/pop_order.cpp
@@ -4,22 +4,22 @@ using namespace std;
 // Based on the number on the first line in the input and the popped numbers on
 // the second line infer the operation sequence
 
-int popped, lower, n, top, s[100001];
+int n, top, x, l, s[100001];
 
 int main() {
   scanf("%d", &n);
-  lower = 0;
+  l = 0;
   top = 0;
-  for (int i = 1; i <= n; ++i) {
-    scanf("%d", &popped);
-    if (s[top] != popped) {
-      for (int j = lower + 1; j <= popped; ++j) {
-        s[top] = j;
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &x);
+    if (x != s[top]) {
+      for (int j = l + 1; j <= x; j++) {
+        s[++top] = j;
         printf("push %d\n", j);
       }
     }
-    printf("pop\n");
     --top;
-    lower = popped;
+    printf("pop\n");
+    l = x;
   }
 }
